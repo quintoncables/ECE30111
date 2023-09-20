f5a58db4970fba8bcc4881e5b9d6e15ea7f2000d
quintoncables <46767928+quintoncables@users.noreply.github.com>
2023-09-20 10:19:18 -0400

    updates math problem answer generation

--- mathClass.py
+++ mathClass.py
@@ -3,11 +3,28 @@ from main import mathMode
 import main
 class mathProblem:
     def __init__(self, mode):
-        self.num1 = random.randint(1, 12)
-        self.num2 = random.randint(1, 12)
         if mode == mathMode.add:
+            self.num1 = random.randint(1, 100)
+            self.num2 = random.randint(1, 100)
             self.answer = self.num1+ self.num2
-        #write answer generating stuff later
+        if mode == mathMode.sub:
+            self.num1 = random.randint(2, 100)
+            self.num2 = random.randint(1, 100)
+            while self.num2 > self.num1:
+                self.num2 = random.randint(1, 100)
+            self.answer = self.num1 - self.num2
+        if mode == mathMode.mult:
+            self.num1 = random.randint(1, 12)
+            self.num2 = random.randint(1, 12)
+            self.answer = self.num1 * self.num2
+        if mode == mathMode.div:
+            self.num1 = random.randint(1, 100)
+            self.num2 = random.randint(1, 100)
+            while ((self.num1 % self.num2)!=0):
+                self.num1 = random.randint(1, 100)
+                self.num2 = random.randint(1, 100)
+            self.answer = self.num1 / self.num2
+      
     def displayProblem(self):
         print("display problem")
         #this will display the problem prompt on the LCD
@@ -17,6 +34,8 @@ class mathProblem:
             return True
         else:
             return False
+    
+   
         
         
     

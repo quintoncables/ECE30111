561adbac4691e8c3a87b637369051c45b10648c1
quintoncables <46767928+quintoncables@users.noreply.github.com>
2023-11-05 17:55:50 -0500

    Update readOrientation.py

--- readOrientation.py
+++ readOrientation.py
@@ -1,5 +1,17 @@
-from main import mainMode
+from modes import  *
+#from constants import *
+from microbit import sleep
+import main
+from microbit import accelerometer
 def readOrientation():
     #sets the main operating mode according to the orientation
-    print("setting the orientation")
+    if accelerometer.is_gesture('face up'):
+        #print("face up")
+        main.activeMode= mainMode.math
+    elif accelerometer.is_gesture('face down'):
+        #print("face down")
+        main.activeMode = mainMode.music
+    else:
+        main.activeMode = mainMode.neither
+
     

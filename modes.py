da3cf58446656ab37391113a1bad22200e380acd
quintoncables <46767928+quintoncables@users.noreply.github.com>
2023-11-05 17:58:05 -0500

    Create modes.py

---
+++ modes.py
@@ -0,0 +1,11 @@
+from enum import *
+class mainMode(enum):
+    neither = 0
+    music = 1
+    math = 2
+class mathMode (enum):
+    neither = 0
+    add = 1
+    sub = 2
+    mult = 3
+    div = 4
